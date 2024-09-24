ffab02676e55705ffcadf2d4ffb4bda314c3ec1a
Liu <Liurxiao@gmail.com>
2024-09-24 09:43:42 -0700

    E4U

--- mainControl.py
+++ mainControl.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     # User options during the run: any number can be true.
     useModelPredictions = False or trainModel  # Apply the learning algorithm to decode the signals.
     plotStreamedData = True  # Graph the data to show incoming signals.
-    useTherapyData = True  # Use the Therapy Data folder for any files.`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+    useTherapyData = True  # Use the Therapy Data folder for any files.
 
     # Specify the user parameters.
     userName = "Testing".replace(" ", "")
