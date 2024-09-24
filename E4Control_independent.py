de1bf2c977d686dbda0a3dc467f916d9a695b422
Liu <Liurxiao@gmail.com>
2024-09-24 13:25:50 -0700

    E4U

--- E4Control_independent.py
+++ E4Control_independent.py
@@ -5,6 +5,7 @@ from helperFiles.dataAcquisitionAndAnalysis import E4StreamingProtocols  # Impor
 
 # Add the directory of the current file to the Python path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 if __name__ == '__main__':
     # specify if using the E4 watch for streaming
     E4StreamingIndicator = True
@@ -16,7 +17,18 @@ if __name__ == '__main__':
         server_port = 28000
         device_id = 'B516C6'
         buffer_size = 4096
-        output_file = "E4_data.xlsx"  # Specify the output file name
+
+        # Define the path to save the file in the e4WatchData folder within _experimentalData
+        script_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current file
+        experimental_data_folder = os.path.join(script_dir, "_experimentalData")  # Path to _experimentalData folder
+        e4_watch_data_folder = os.path.join(experimental_data_folder, "e4WatchData")  # Path to e4WatchData folder
+
+        # Ensure the e4WatchData folder exists
+        os.makedirs(e4_watch_data_folder, exist_ok=True)
+
+        # Set the output file path to save in the e4WatchData folder
+        output_file = os.path.join(e4_watch_data_folder, "E4_data.xlsx")
+
         e4_streamer = E4StreamingProtocols.E4Streaming(server_address, server_port, device_id, buffer_size, output_file, plotStreamedData)
         e4_streamer.connect()
         e4_streamer.subscribe_to_data()
