27e172eab02e331f67ee77075d13dc9859e2f5f3
Samuel Solomon <sasolomon1998@gmail.com>
2024-09-26 15:40:52 -0700

    E4dataupdate

--- E4Control_independent.py
+++ E4Control_independent.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         os.makedirs(e4_watch_data_folder, exist_ok=True)
 
         # Set the output file path to save in the e4WatchData folder
-        output_file = os.path.join(e4_watch_data_folder, "E4_data_baseline_featureAnalysis.xlsx")
+        output_file = os.path.join(e4_watch_data_folder, "20240926_E4_data_Testing.xlsx")
 
         e4_streamer = E4StreamingProtocols.E4Streaming(server_address, server_port, device_id, buffer_size, output_file, plotStreamedData)
         e4_streamer.connect()
