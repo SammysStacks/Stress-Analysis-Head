a14f7b64ba1f57460823e25fd08dd268eaa63764
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2024-10-21 06:38:24 -0700

    HEAD

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/trainingProtocolHelpers.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/trainingProtocolHelpers.py
@@ -32,7 +32,7 @@ class trainingProtocolHelpers:
             numEpochs = len(modelLosses)
 
             # Check if we should add a new layer.
-            # if numEpochs != 0 and numEpochs % 10 != 0: return None
+            if numEpochs != 0 and numEpochs % 10 != 0: return None
             if modelLosses[-1] < 0.01: return None
 
         # Add a new layer to the model.
