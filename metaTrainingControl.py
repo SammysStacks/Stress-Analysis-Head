7f26797c228457975c237198bcbe6da95edf1b3b
Samuel Solomon <sasolomon1998@gmail.com>
2024-10-21 01:56:49 -0700

    U

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -62,11 +62,11 @@ if __name__ == "__main__":
 
     # Add arguments for the neural operator.
     parser.add_argument('--operatorType', type=str, default='wavelet', help='The type of operator to use for the neural operator: wavelet')
-    parser.add_argument('--waveletType', type=str, default='bior2.2', help='The wavelet type for the wavelet transform: bior3.7, db3, dmey, etc')
+    parser.add_argument('--waveletType', type=str, default='bior3.7', help='The wavelet type for the wavelet transform: bior3.7, db3, dmey, etc')
 
     # Add arguments for the signal encoder prediction
-    parser.add_argument('--numSpecificEncodingLayers', type=int, default=2, help='The number of layers in the specific signal encoding neural operator.')
-    parser.add_argument('--numMetaEncodingLayers', type=int, default=8, help='The number of layers in the shared signal encoding operator.')
+    parser.add_argument('--numSpecificEncodingLayers', type=int, default=4, help='The number of layers in the specific signal encoding neural operator.')
+    parser.add_argument('--numMetaEncodingLayers', type=int, default=16, help='The number of layers in the shared signal encoding operator.')
     parser.add_argument('--encodedDimension', type=int, default=256, help='The dimension of the encoded signal.')
 
     # Add arguments for the emotion prediction
