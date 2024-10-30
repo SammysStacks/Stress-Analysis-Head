b9ea2b89e886c9cb6cba42eadd06a9416f977e5c
Samuel Solomon <sasolomon1998@gmail.com>
2024-10-30 15:19:05 -0700

    U

--- helperFiles/machineLearning/dataInterface/compileModelDataHelpers.py
+++ helperFiles/machineLearning/dataInterface/compileModelDataHelpers.py
@@ -4,7 +4,6 @@ import pickle
 
 import numpy as np
 import torch
-from matplotlib import pyplot as plt
 
 from helperFiles.machineLearning.modelControl.Models.pyTorch.emotionModelInterface.emotionModel.emotionModelHelpers.emotionDataInterface import emotionDataInterface
 from helperFiles.machineLearning.modelControl.Models.pyTorch.emotionModelInterface.emotionModel.emotionModelHelpers.generalMethods.dataAugmentation import dataAugmentation
@@ -252,14 +251,11 @@ class compileModelDataHelpers:
         minLowerBoundaryMask = 1 < (biomarkerData < -modelConstants.minMaxScale + 0.25).sum(dim=-1)  # Number of points below -0.95: batchSize, numSignals
         minUpperBoundaryMask = 1 < (modelConstants.minMaxScale - 0.25 < biomarkerData).sum(dim=-1)  # Number of points above 0.95: batchSize, numSignals
         averageDiff = biomarkerData.diff(dim=-1).abs().mean(dim=-1) < self.maxAverageDiff  # Average difference between consecutive points: batchSize, numSignals
-        startValueMask = (biomarkerData[:, :, 0:5].abs() <= modelConstants.minMaxScale - 0.1).any(dim=-1)  # Start value: batchSize, numSignals
-        endValueMask = (biomarkerData[:, :, -5:].abs() <= modelConstants.minMaxScale - 0.1).any(dim=-1)  # End value: batchSize, numSignals
-        maxDiff = biomarkerData.diff(dim=-1).abs().max(dim=-1).values < self.maxDiff  # Maximum difference between consecutive points: batchSize, numSignals
         minPointsMask = self.minSequencePoints <= allNumSignalPoints  # Minimum number of points: batchSize, numSignals
         validSignalMask = validDataMask.any(dim=-1)  # Missing data: batchSize, numSignals
 
         # Combine all masks into a single mask and expand to match dimensions.
-        validSignalMask = minPointsMask & endValueMask & startValueMask & minLowerBoundaryMask & minUpperBoundaryMask & averageDiff & validSignalMask & maxDiff
+        validSignalMask = minPointsMask & minLowerBoundaryMask & minUpperBoundaryMask & averageDiff & validSignalMask
         validSignalInds = self.minSignalPresentCount < validSignalMask.sum(dim=0)
 
         # Filter out the invalid signals
