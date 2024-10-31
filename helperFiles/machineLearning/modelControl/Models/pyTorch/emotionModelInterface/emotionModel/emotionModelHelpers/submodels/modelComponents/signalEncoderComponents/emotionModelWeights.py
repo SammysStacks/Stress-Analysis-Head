0cdf9215152c8543f5f9258fba81d483ae4a4d8c
Samuel Solomon <sasolomon1998@gmail.com>
2024-10-31 15:40:30 -0700

    U

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/signalEncoderComponents/emotionModelWeights.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/signalEncoderComponents/emotionModelWeights.py
@@ -37,9 +37,9 @@ class emotionModelWeights(convolutionalHelpers):
     def getInitialPhysiologicalProfile(numExperiments, encodedDimension):
         # Initialize the physiological profile.
         physiologicalProfile = torch.randn(numExperiments, encodedDimension, dtype=torch.float64)
-        physiologicalProfile = physiologicalProfile - physiologicalProfile.min(dim=-1, keepdim=True).values
-        physiologicalProfile = physiologicalProfile / physiologicalProfile.max(dim=-1, keepdim=True).values
-        physiologicalProfile = 2*physiologicalProfile - 1
+        # physiologicalProfile = physiologicalProfile - physiologicalProfile.min(dim=-1, keepdim=True).values
+        # physiologicalProfile = physiologicalProfile / physiologicalProfile.max(dim=-1, keepdim=True).values
+        # physiologicalProfile = 2*physiologicalProfile - 1
 
         # Initialize the physiological profile as a parameter.
         physiologicalProfile = nn.Parameter(physiologicalProfile)
