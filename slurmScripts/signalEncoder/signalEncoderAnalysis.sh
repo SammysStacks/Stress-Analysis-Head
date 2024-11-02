3acee4f51aecd64f643f2d123c17709caeebeb31
Samuel Solomon <sasolomon1998@gmail.com>
2024-11-01 17:39:20 -0700

    U

--- slurmScripts/signalEncoder/signalEncoderAnalysis.sh
+++ slurmScripts/signalEncoder/signalEncoderAnalysis.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 start=$(date +%s)
 
+# Get the value of signalEncoderModel from modelConstants.py
+signalEncoderModel=$(python -c "from modelConstants import signalEncoderModel; print(signalEncoderModel)")
+
 # Pass the parameters to the Python script
 srun accelerate launch ./../../metaTrainingControl.py \
     --numSignalEncoderLayers "$1" \
     --goldenRatio "$2" \
     --encodedDimension "$3" \
     --deviceListed "HPC-$4" \
-    --submodel modelConstants.signalEncoderModel \
+    --submodel "$signalEncoderModel" \
     --waveletType "$5" \
-#    --optimizerType "$6" \
+#    --optimizerType "$6"
 
 end=$(date +%s)
 runtime=$((end-start))
