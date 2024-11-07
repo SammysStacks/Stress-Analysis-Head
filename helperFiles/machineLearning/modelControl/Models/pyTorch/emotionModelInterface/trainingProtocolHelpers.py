731a172973905feaac0dbe6a38c208d3506f96b4
Samuel Solomon <sasolomon1998@gmail.com>
2024-11-06 19:27:50 -0800

    U

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/trainingProtocolHelpers.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/trainingProtocolHelpers.py
@@ -51,7 +51,7 @@ class trainingProtocolHelpers:
 
             # Train the updated model.
             self.modelMigration.unifyModelWeights(allModels=[modelPipeline], modelWeights=self.sharedModelWeights, layerInfo=self.unifiedLayerData)
-            modelPipeline.trainModel(dataLoader, submodel, inferenceTraining=False, profileTraining=False, specificTraining=True, trainSharedLayers=True, numEpochs=1)   # Full model training.
+            modelPipeline.trainModel(dataLoader, submodel, inferenceTraining=False, profileTraining=True, specificTraining=True, trainSharedLayers=True, numEpochs=1)   # Full model training.
             self.accelerator.wait_for_everyone()
 
             # Store the new model weights.
