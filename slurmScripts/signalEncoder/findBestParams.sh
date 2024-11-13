68a5415c03c1479d7c176f9709b54316f7a94824
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2024-11-12 20:41:32 -0800

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-waveletTypes=('bior6.8')
+waveletTypes=('bior3.7')
 optimizers=('RAdam')
 encodedDimensions=(128)
-signalEncoderLayers=(4 8 16 32)
+signalEncoderLayers=(1 2 4 8 16 32)
 weightDecays=(0.0001)
-goldenRatios=(1 2 4 16)
-lrs=(0.01 0.1)
+goldenRatios=(1 2 4 8 16 32)
+lrs=(0.01)
 
 for optimizer in "${optimizers[@]}"
 do
