f0d66b0bfcde49adae3cc3d07841a66421c7c991
Samuel Solomon <sasolomon1998@gmail.com>
2024-11-16 17:38:07 -0800

    U

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-waveletTypes=('bior2.2' 'bior3.7')
+waveletTypes=('bior1.1' 'bior2.2' 'bior3.7')
 optimizers=('AdamW')
 encodedDimensions=(128 256)
-signalEncoderLayers=(8 16 32)
+signalEncoderLayers=(8 16 32 48)
 goldenRatios=(1 2 4 8)
 weightDecays=(0)
 lrs=(0.01)
