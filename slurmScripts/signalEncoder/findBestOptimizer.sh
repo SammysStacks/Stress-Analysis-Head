cf9a0bc082bb1278ef16aca5bf243d62c1dba31a
Samuel Solomon <sasolomon1998@gmail.com>
2024-11-20 14:33:57 -0800

    U

--- slurmScripts/signalEncoder/findBestOptimizer.sh
+++ slurmScripts/signalEncoder/findBestOptimizer.sh
@@ -2,14 +2,19 @@
 
 optimizers=('Adadelta' 'Adam' 'AdamW' 'NAdam' 'RAdam' 'Adamax' 'ASGD' 'RMSprop' 'Rprop' 'SGD')
 
-numSignalEncoderLayers=8
+numSignalEncoderLayers=16
 waveletType='bior3.7'
-encodedDimension=128
-goldenRatio=4
+encodedDimension=256
+goldenRatio=2
 
 for optimizer in "${optimizers[@]}"
 do
-    echo "Submitting job with $numSignalEncoderLayers numSignalEncoderLayers $goldenRatio goldenRatio $encodedDimension encodedDimension on $1 using $waveletType waveletType and $optimizer optimizer."
+    # Check if goldenRatio is greater than numSignalEncoderLayers
+    if [ "$goldenRatio" -gt "$numSignalEncoderLayers" ]; then
+      continue  # Skip this iteration if the condition is true
+    fi
+
+    echo "Submitting job with $numSignalEncoderLayers numSignalEncoderLayers, $goldenRatio goldenRatio, $encodedDimension encodedDimension, $waveletType waveletType, $optimizer optimizer on $1"
 
     if [ "$1" == "CPU" ]; then
         sbatch -J "signalEncoder_numSignalEncoderLayers_${numSignalEncoderLayers}_goldenRatio_${goldenRatio}_encodedDimension_${encodedDimension}_${waveletType}_${optimizer}_$1" submitSignalEncoder_CPU.sh "$numSignalEncoderLayers" "$goldenRatio" "$encodedDimension" "$1" "$waveletType" "$optimizer"
