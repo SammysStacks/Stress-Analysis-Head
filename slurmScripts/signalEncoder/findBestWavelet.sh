06160c696e0e7adb3f477739e4c3f7a7444d4654
Samuel Solomon <sasolomon1998@gmail.com>
2024-11-21 10:18:49 -0800

    U

--- slurmScripts/signalEncoder/findBestWavelet.sh
+++ slurmScripts/signalEncoder/findBestWavelet.sh
@@ -25,6 +25,10 @@ waveletTypes=( \
 )
 
 waveletTypes=( \
+    # 17 coif wavelets
+    'coif1' 'coif2' 'coif3' 'coif4' 'coif5' 'coif6' 'coif7' 'coif8' 'coif9' 'coif10' \
+    'coif11' 'coif12' 'coif13' 'coif14' 'coif15' 'coif16' 'coif17' \
+
     # 15 rbio wavelets
     'rbio1.1' 'rbio1.3' 'rbio1.5' 'rbio2.2' 'rbio2.4' 'rbio2.6' 'rbio2.8' \
     'rbio3.1' 'rbio3.3' 'rbio3.5' 'rbio3.7' 'rbio3.9' 'rbio4.4' 'rbio5.5' 'rbio6.8' \
@@ -32,10 +36,6 @@ waveletTypes=( \
     # 15 bior wavelets
     'bior1.1' 'bior1.3' 'bior1.5' 'bior2.2' 'bior2.4' 'bior2.6' 'bior2.8' \
     'bior3.1' 'bior3.3' 'bior3.5' 'bior3.7' 'bior3.9' 'bior4.4' 'bior5.5' 'bior6.8' \
-
-    # 17 coif wavelets
-    'coif1' 'coif2' 'coif3' 'coif4' 'coif5' 'coif6' 'coif7' 'coif8' 'coif9' 'coif10' \
-    'coif11' 'coif12' 'coif13' 'coif14' 'coif15' 'coif16' 'coif17' \
 )
 
 numSignalEncoderLayers=16
