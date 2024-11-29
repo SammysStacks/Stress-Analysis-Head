16bd92aad7a6da7871354c9136d67c59d3819214
Samuel Solomon <sasolomon1998@gmail.com>
2024-11-29 12:58:30 -0800

    Lab

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -3,11 +3,11 @@
 numSpecificEncoderLayers=(1 2 4)
 signalEncoderLayers=(4 8 12 16)
 encodedDimensions=(256)
-waveletTypes=('bior2.2' 'bior3.1' 'bior3.3' 'bior3.5' 'bior3.7')
+waveletTypes=('bior3.1' 'bior3.3' 'bior3.5')
 optimizers=('RAdam')
 
 lr_general=0.001
-lr_physio=0.01
+lr_physio=0.1
 
 for optimizer in "${optimizers[@]}"
 do
