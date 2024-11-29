4042f2fe5ce55f27e2f8c23d82e2c8ff689d242d
Samuel Solomon <sasolomon1998@gmail.com>
2024-11-29 14:12:33 -0800

    Lab

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-waveletTypes=('bior3.1' 'bior3.3' 'bior3.5')
-numSpecificEncoderLayers=(1 2 4)
-signalEncoderLayers=(4 8 12 16)
-lrs_general=(0.001 0.0001)
-encodedDimensions=(256)
-lrs_physio=(0.1 0.01)
-optimizers=('RAdam')
+waveletTypes_arr=('bior3.1' 'bior3.3' 'bior3.5')
+numSpecificEncoderLayers_arr=(2 4)
+signalEncoderLayers_arr=(4 8 12 16)
+lrs_general=('0.001' '0.0001')
+encodedDimensions_arr=(256)
+lrs_physio=('0.1' '0.01')
+optimizers_arr=('RAdam')
 
 for lr_physio in "${lrs_physio[@]}"
 do
   for lr_general in "${lrs_general[@]}"
   do
-    for optimizer in "${optimizers[@]}"
+    for optimizer in "${optimizers_arr[@]}"
     do
-      for waveletType in "${waveletTypes[@]}"
+      for waveletType in "${waveletTypes_arr[@]}"
       do
-        for encodedDimension in "${encodedDimensions[@]}"
+        for encodedDimension in "${encodedDimensions_arr[@]}"
         do
-          for numSpecificEncoderLayers in "${numSpecificEncoderLayers[@]}"
+          for numSpecificEncoderLayers in "${numSpecificEncoderLayers_arr[@]}"
           do
-            for numSharedEncoderLayers in "${signalEncoderLayers[@]}"
+            for numSharedEncoderLayers in "${signalEncoderLayers_arr[@]}"
             do
               # Check if numSpecificEncoderLayers is greater than half the numSharedEncoderLayers
               if [ $((2 * numSpecificEncoderLayers)) -gt "$numSharedEncoderLayers" ]; then
                 continue  # Skip this iteration if the condition is true
               fi
 
-              echo "Submitting job with $numSharedEncoderLayers numSharedEncoderLayers, $numSpecificEncoderLayers numSpecificEncoderLayers, $encodedDimension encodedDimension, $waveletType waveletType, $optimizer optimizer on $1"
+              echo "Submitting job with $numSharedEncoderLayers numSharedEncoderLayers, $numSpecificEncoderLayers numSpecificEncoderLayers, $encodedDimension encodedDimension, $waveletType waveletType, $optimizer optimizer, $lr_physio lr_physio, $lr_general lr_general"
 
               if [ "$1" == "CPU" ]; then
                   sbatch -J "signalEncoder_numSharedEncoderLayers_${numSharedEncoderLayers}_numSpecificEncoderLayers_${numSpecificEncoderLayers}_encodedDimension_${encodedDimension}_${waveletType}_${optimizer}_$1" submitSignalEncoder_CPU.sh "$numSharedEncoderLayers" "$numSpecificEncoderLayers" "$encodedDimension" "$1" "$waveletType" "$optimizer" "$lr_physio" "$lr_general"
