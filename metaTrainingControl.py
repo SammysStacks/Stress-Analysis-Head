b76fac12d8686d57c30b9bc6268c03abdf7511a8
Samuel Solomon <sasolomon1998@gmail.com>
2024-11-30 16:28:07 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     parser.add_argument('--deviceListed', type=str, default=accelerator.device.type, help='The device we are using: cpu, cuda')
 
     # Add arguments for the signal encoder architecture.
-    parser.add_argument('--numSpecificEncoderLayers', type=int, default=1, help='The number of layers in the model.')
+    parser.add_argument('--numSpecificEncoderLayers', type=int, default=2, help='The number of layers in the model.')
     parser.add_argument('--numSharedEncoderLayers', type=int, default=8, help='The number of layers in the model.')
     parser.add_argument('--encodedDimension', type=int, default=256, help='The dimension of the encoded signal.')
  
