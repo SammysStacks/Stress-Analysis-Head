991c6fdb3e379b594ed4f8a90f5c12d477f48dd1
Samuel Solomon <sasolomon1998@gmail.com>
2024-12-06 15:22:09 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -52,9 +52,9 @@ if __name__ == "__main__":
 
     # Add arguments for the signal encoder architecture.
     parser.add_argument('--numSpecificEncoderLayers', type=int, default=1, help='The number of layers in the model.')
-    parser.add_argument('--numSharedEncoderLayers', type=int, default=8, help='The number of layers in the model.')
-    parser.add_argument('--encodedDimension', type=int, default=128, help='The dimension of the encoded signal.')
-    parser.add_argument('--numEncodedWeights', type=int, default=32, help='The number of profile weights.')
+    parser.add_argument('--numSharedEncoderLayers', type=int, default=12, help='The number of layers in the model.')
+    parser.add_argument('--encodedDimension', type=int, default=256, help='The dimension of the encoded signal.')
+    parser.add_argument('--numEncodedWeights', type=int, default=64, help='The number of profile weights.')
  
     # Add arguments for the neural operator.
     parser.add_argument('--operatorType', type=str, default='wavelet', help='The type of operator to use for the neural operator: wavelet')
