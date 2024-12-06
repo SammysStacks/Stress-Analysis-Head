93f2479f60821f946670e1b089bc0a21f5ed4a1d
Samuel Solomon <sasolomon1998@gmail.com>
2024-12-06 09:08:01 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     )
 
     # General model parameters.
-    trainingDate = "2024-12-05"  # The current date we are training the model. Unique identifier of this training set.
+    trainingDate = "2024-12-06"  # The current date we are training the model. Unique identifier of this training set.
     testSplitRatio = 0.1  # The percentage of testing points.
 
     # ----------------------- Parse Model Parameters ----------------------- #
