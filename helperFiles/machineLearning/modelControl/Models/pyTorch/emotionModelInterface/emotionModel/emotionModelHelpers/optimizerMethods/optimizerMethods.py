1b65cf92f0235d79af3ba399e963ecca52a4327e
Samuel Solomon <sasolomon1998@gmail.com>
2024-12-06 12:40:29 -0800

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/optimizerMethods/optimizerMethods.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/optimizerMethods/optimizerMethods.py
@@ -14,7 +14,7 @@ class optimizerMethods:
     def getModelParams(self, submodel, model):
         modelParams = [
             # Specify the model parameters for the signal encoding.
-            {'params': model.inferenceModel.parameters(), 'weight_decay': 1e-4, 'lr': 0.1},
+            {'params': model.inferenceModel.parameters(), 'weight_decay': 1e-2, 'lr': 0.1},
             {'params': model.sharedSignalEncoderModel.parameters(), 'weight_decay': 1e-2, 'lr': self.userInputParams['sharedLR']},  # 0.1 - 0.001
             {'params': (param for name, param in model.specificSignalEncoderModel.named_parameters() if "profileModel" not in name), 'weight_decay': 1e-2, 'lr': self.userInputParams['generalLR']},  # 0.1 - 0.001
             {'params': model.specificSignalEncoderModel.profileModel.parameters(), 'weight_decay': 1e-2, 'lr': self.userInputParams['physioLR']},  # 0.1 - 0.01
