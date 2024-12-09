6ebbb0fd41f6d2f411d12bcb26b4129f0f804086
Samuel Solomon <sasolomon1998@gmail.com>
2024-12-08 19:37:11 -0800

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelParameters.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelParameters.py
@@ -66,8 +66,8 @@ class modelParameters:
 
     @staticmethod
     def getInferenceBatchSize(submodel, device):
-        if submodel == modelConstants.signalEncoderModel: return 256 if device == "cpu" else 256
-        elif submodel == modelConstants.emotionModel: return 256 if device == "cpu" else 256
+        if submodel == modelConstants.signalEncoderModel: return 128 if device == "cpu" else 128
+        elif submodel == modelConstants.emotionModel: return 128 if device == "cpu" else 128
         else: raise Exception()
 
     @staticmethod
