0849e896ffc9578ababbdd6e72fb160ec81dcfb6
Samuel Solomon <sasolomon1998@gmail.com>
2024-12-09 01:43:43 -0800

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelVisualizations/modelVisualizations.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelVisualizations/modelVisualizations.py
@@ -103,7 +103,7 @@ class modelVisualizations(globalPlottingProtocols):
 
                     # Plot the signal encoding training information.
                     self.signalEncoderViz.plotSignalEncodingStatePath(physiologicalTimes, compiledSignalEncoderLayerStates, epoch=currentEpoch, saveFigureLocation="signalEncoding/", plotTitle="Signal Encoding State Path")
-                    if compiledSignalEncoderLayerStatePath.shape[0] != 0: self.signalEncoderViz.plotProfilePath(physiologicalTimes=physiologicalTimes, physiologicalProfile=compiledSignalEncoderLayerStates[:, -1, :, 0, :], profileStatePath=compiledSignalEncoderLayerStatePath[:, :, 0, :], epoch=currentEpoch, saveFigureLocation="signalEncoding/", plotTitle="Layer State Path")
+                    if compiledSignalEncoderLayerStatePath.shape[0] != 0: self.signalEncoderViz.plotProfilePath(physiologicalTimes=physiologicalTimes, physiologicalProfile=compiledSignalEncoderLayerStates[-1, :, 0, :], profileStatePath=compiledSignalEncoderLayerStatePath[:, -1, :, 0, :], epoch=currentEpoch, saveFigureLocation="signalEncoding/", plotTitle="Last Layer State Path")
 
                 # Plot the autoencoder results.
                 self.signalEncoderViz.plotEncoder(signalData, reconstructedSignalData, physiologicalTimes, resampledSignalData, epoch=currentEpoch, saveFigureLocation="signalReconstruction/", plotTitle="Signal Encoding Reconstruction", numSignalPlots=1)
