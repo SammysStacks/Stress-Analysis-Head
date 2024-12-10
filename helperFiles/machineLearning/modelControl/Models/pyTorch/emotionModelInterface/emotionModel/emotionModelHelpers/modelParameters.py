2e2e8891f3d262fbe99f6d78c049dcae7fa9b38b
Samuel Solomon <sasolomon1998@gmail.com>
2024-12-10 15:08:28 -0800

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelParameters.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelParameters.py
@@ -71,7 +71,7 @@ class modelParameters:
         else: raise Exception()
 
     @staticmethod
-    def getEpochInfo(): return 150, 10, 50  # numEpochs, numEpoch_toPlot, numEpoch_toSaveFull
+    def getEpochInfo(): return 1000, 10, 50  # numEpochs, numEpoch_toPlot, numEpoch_toSaveFull
 
     @staticmethod
     def getProfileEpochs(): return modelConstants.userInputParams['numProfileEpochs']  # numEpochs
