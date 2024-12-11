03a774ea55606c789c9cfd6ad0260a35296138fa
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2024-12-11 15:32:01 -0800

    HEAD

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -46,14 +46,14 @@ if __name__ == "__main__":
     # Add arguments for the general model
     parser.add_argument('--submodel', type=str, default=modelConstants.signalEncoderModel, help='The component of the model we are training. Options: signalEncoderModel, emotionModel')
     parser.add_argument('--optimizerType', type=str, default='AdamW', help='The optimizerType used during training convergence: Options: RMSprop, Adam, AdamW, SGD, etc.')
-    parser.add_argument('--reversibleLearningProtocol', type=str, default='rCNN', help='The learning protocol for the model: rCNN')
+    parser.add_argument('--reversibleLearningProtocol', type=str, default='FC', help='The learning protocol for the model: rCNN')
     parser.add_argument('--irreversibleLearningProtocol', type=str, default='FC', help='The learning protocol for the model: CNN, FC')
     parser.add_argument('--deviceListed', type=str, default=accelerator.device.type, help='The device we are using: cpu, cuda')
 
     # Add arguments for the signal encoder architecture.
     parser.add_argument('--numSpecificEncoderLayers', type=int, default=1, help='The number of layers in the model.')
-    parser.add_argument('--numSharedEncoderLayers', type=int, default=8, help='The number of layers in the model.')
-    parser.add_argument('--encodedDimension', type=int, default=256, help='The dimension of the encoded signal.')
+    parser.add_argument('--numSharedEncoderLayers', type=int, default=1, help='The number of layers in the model.')
+    parser.add_argument('--encodedDimension', type=int, default=64, help='The dimension of the encoded signal.')
     parser.add_argument('--numProfileEpochs', type=int, default=25, help='The epochs for profile training.')
     parser.add_argument('--numEncodedWeights', type=int, default=32, help='The number of profile weights.')
 
