b37ab2a3fa080e9077e14753c05acdd1f610974d
Samuel Solomon <sasolomon1998@gmail.com>
2024-12-14 13:53:48 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     parser.add_argument('--numSpecificEncoderLayers', type=int, default=1, help='The number of layers in the model.')
     parser.add_argument('--numSharedEncoderLayers', type=int, default=8, help='The number of layers in the model.')
     parser.add_argument('--encodedDimension', type=int, default=256, help='The dimension of the encoded signal.')
-    parser.add_argument('--numProfileEpochs', type=int, default=30, help='The epochs for profile training.')
+    parser.add_argument('--numProfileEpochs', type=int, default=5, help='The epochs for profile training.')
     parser.add_argument('--numEncodedWeights', type=int, default=32, help='The number of profile weights.')
 
     # Add arguments for the neural operator.
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     parser.add_argument('--numActivityChannels', type=int, default=4, help='The number of activity channels.')
 
     # Temporary parameters.
-    parser.add_argument('--profileLR', type=float, default=0.25, help='The learning rate of the physiological model.')
+    parser.add_argument('--profileLR', type=float, default=0.2, help='The learning rate of the physiological model.')
     parser.add_argument('--profileWD', type=float, default=0, help='The learning rate of the general model.')
 
     # Temporary parameters.
