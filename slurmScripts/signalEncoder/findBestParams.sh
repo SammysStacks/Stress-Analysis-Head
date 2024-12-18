1df8228714c9caa18059c987c93b84d6f5b649e4
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2024-12-18 15:33:06 -0800

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -12,7 +12,7 @@ wds_reversible=('1e-4')
 
 # General parameters: 320
 uniformWeightLimits_arr=('0.2' '0.25' '1/3'  '0.5' '0.75' '1')  # 10
-signalEncoderLayers_arr=(2 4 6 8)  # 1 2 3 4 5 6 7 8
+signalEncoderLayers_arr=(6 8 10)  # 1 2 3 4 5 6 7 8
 encodedDimensions_arr=(128 256 512)  # 64 128 256 512
 allNumEncodedWeights=(32 64 128 256)  # 4 8 16 32 64
 numSpecificEncoderLayers_arr=(1 2)  # 1 2
