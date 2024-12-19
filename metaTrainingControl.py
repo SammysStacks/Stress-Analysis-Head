a95f343f1d183d576745c61d88fa535bffe4c14b
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2024-12-18 16:34:34 -0800

    HEAD

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -52,11 +52,11 @@ if __name__ == "__main__":
 
     # Add arguments for the signal encoder architecture.
     parser.add_argument('--numSpecificEncoderLayers', type=int, default=1, help='The number of layers in the model.')
-    parser.add_argument('--numSharedEncoderLayers', type=int, default=6, help='The number of layers in the model.')
+    parser.add_argument('--numSharedEncoderLayers', type=int, default=4, help='The number of layers in the model.')
     parser.add_argument('--uniformWeightLimits', type=float, default=0.28, help='The limits for profile initialization.')
     parser.add_argument('--encodedDimension', type=int, default=256, help='The dimension of the encoded signal.')
     parser.add_argument('--numProfileEpochs', type=int, default=30, help='The epochs for profile training.')
-    parser.add_argument('--numEncodedWeights', type=int, default=64, help='The number of profile weights.')
+    parser.add_argument('--numEncodedWeights', type=int, default=128, help='The number of profile weights.')
 
     # Add arguments for the neural operator.
     parser.add_argument('--operatorType', type=str, default='wavelet', help='The type of operator to use for the neural operator: wavelet')
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     parser.add_argument('--numActivityChannels', type=int, default=4, help='The number of activity channels.')
 
     # Signal encoder learning rates.
-    parser.add_argument('--profileLR', type=float, default=0.25, help='The learning rate of the physiological model.')
+    parser.add_argument('--profileLR', type=float, default=0.3, help='The learning rate of the physiological model.')
     parser.add_argument('--reversibleLR', type=float, default=1e-3, help='The learning rate of the general model.')
     parser.add_argument('--physGenLR', type=float, default=1e-4, help='The learning rate of the general model.')
 
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     parser.add_argument('--physGenWD', type=float, default=1e-5, help='The learning rate of the general model.')
 
     # Add arguments for the emotion and activity architecture.
-    parser.add_argument('--momentum_decay', type=float, default=0.004, help='Momentum decay for the optimizer.')
+    parser.add_argument('--momentum_decay', type=float, default=0.01, help='Momentum decay for the optimizer.')
     parser.add_argument('--beta1', type=float, default=0.7, help='Beta1 for the optimizer: 0.7 -> 0.9')
     parser.add_argument('--beta2', type=float, default=0.9, help='Beta2 for the optimizer: 0.9 -> 0.999')
 
