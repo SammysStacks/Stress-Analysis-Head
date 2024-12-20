747e6e17d7f8d67b062e0319d79ca22cbcaec35b
Samuel Solomon <sasolomon1998@gmail.com>
2024-12-20 09:45:05 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -51,12 +51,12 @@ if __name__ == "__main__":
     parser.add_argument('--deviceListed', type=str, default=accelerator.device.type, help='The device we are using: cpu, cuda')
 
     # Add arguments for the signal encoder architecture.
-    parser.add_argument('--numSpecificEncoderLayers', type=int, default=1, help='The number of layers in the model.')
-    parser.add_argument('--numSharedEncoderLayers', type=int, default=8, help='The number of layers in the model.')
+    parser.add_argument('--numSpecificEncoderLayers', type=int, default=2, help='The number of layers in the model.')
+    parser.add_argument('--numSharedEncoderLayers', type=int, default=6, help='The number of layers in the model.')
     parser.add_argument('--uniformWeightLimits', type=float, default=0.25, help='The limits for profile initialization.')
     parser.add_argument('--encodedDimension', type=int, default=256, help='The dimension of the encoded signal.')
-    parser.add_argument('--numProfileEpochs', type=int, default=30, help='The epochs for profile training.')
-    parser.add_argument('--numEncodedWeights', type=int, default=128, help='The number of profile weights.')
+    parser.add_argument('--numProfileEpochs', type=int, default=32, help='The epochs for profile training.')
+    parser.add_argument('--numEncodedWeights', type=int, default=32, help='The number of profile weights.')
 
     # Add arguments for the neural operator.
     parser.add_argument('--operatorType', type=str, default='wavelet', help='The type of operator to use for the neural operator: wavelet')
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     parser.add_argument('--numActivityChannels', type=int, default=4, help='The number of activity channels.')
 
     # Signal encoder learning rates.
-    parser.add_argument('--profileLR', type=float, default=0.075, help='The learning rate of the physiological model.')
+    parser.add_argument('--profileLR', type=float, default=0.05, help='The learning rate of the physiological model.')
     parser.add_argument('--reversibleLR', type=float, default=1e-3, help='The learning rate of the general model.')
     parser.add_argument('--physGenLR', type=float, default=1e-4, help='The learning rate of the general model.')
 
