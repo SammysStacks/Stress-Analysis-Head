b9ae219849d376a6fec59cfca504426493475ae7
Samuel Solomon <sasolomon1998@gmail.com>
2025-01-01 04:51:36 -0800

    Updating

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelVisualizations/_signalEncoderVisualizations.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelVisualizations/_signalEncoderVisualizations.py
@@ -374,7 +374,7 @@ class signalEncoderVisualizations(globalPlottingProtocols):
         ax.view_init(elev=30, azim=135)
 
         # Add labels and title
-        ax.set_title(f"3D Visualization of Jacobian Full Pass Path", fontsize=16, weight='bold', pad=20)
+        ax.set_title(plotTitle, fontsize=16, weight='bold', pad=20)
         ax.set_xlabel("Eigenvalue Index", fontsize=12, labelpad=10)
         ax.set_ylabel("Model Layer Index", fontsize=12, labelpad=10)
         ax.set_zlabel("Amplitude", fontsize=12, labelpad=10)
