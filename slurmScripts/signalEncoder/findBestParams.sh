c5d6bd665c531dd2861f80952cb85a66c585ccbb
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-01-03 01:07:15 -0800

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -50,9 +50,9 @@ lrs_profile=('0.01')
 
 
 lrs_profile=('0.01' '0.025' '0.05' '0.1')  # 0.005 <= x <= 0.05
-numSharedEncoderLayers_arr=(4 6 8 10 12)  # 5
-numSpecificEncoderLayers_arr=(1 2 3 4 5 6)  # 6
-numProfileShots_arr=(16 24 32)  # 3
+numSharedEncoderLayers_arr=(4 6 8 12)  # 4
+numSpecificEncoderLayers_arr=(1 2 3 4 5 6)  # 4
+numProfileShots_arr=(24)  # 2
 
 for beta1s in "${beta1s_arr[@]}"
 do
