d330dc98ec407dc0e0ab0f9ce106bf5b3c351b5b
Samuel Solomon <sasolomon1998@gmail.com>
2025-01-04 06:07:33 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     parser.add_argument('--irreversibleLearningProtocol', type=str, default='FC', help='The learning protocol for the model: CNN, FC')
     parser.add_argument('--reversibleLearningProtocol', type=str, default='rCNN', help='The learning protocol for the model: rCNN')
     parser.add_argument('--deviceListed', type=str, default=accelerator.device.type, help='The device we are using: cpu, cuda')
-    parser.add_argument('--encodedDimension', type=int, default=256, help='The dimension of the encoded signal.')
+    parser.add_argument('--encodedDimension', type=int, default=128, help='The dimension of the encoded signal.')
 
     # Add arguments for the neural operator.
     parser.add_argument('--waveletType', type=str, default='bior3.1', help='The wavelet type for the wavelet transform: bior3.1, bior3.3, bior2.2, bior3.5')
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     # ----------------------- Training Parameters ----------------------- #
 
     # Signal encoder learning rates.
-    parser.add_argument('--profileLR', type=float, default=0.025, help='The learning rate of the health model.')
+    parser.add_argument('--profileLR', type=float, default=0.01, help='The learning rate of the health model.')
     parser.add_argument('--reversibleLR', type=float, default=1e-3, help='The learning rate of the general model.')
     parser.add_argument('--physGenLR', type=float, default=1e-4, help='The learning rate of the general model.')
 
@@ -92,16 +92,18 @@ if __name__ == "__main__":
     # Parse the arguments.
     userInputParams = vars(parser.parse_args())
 
+    # TODO
+    userInputParams['infinite'] = userInputParams['beta1']
+    userInputParams['linearity'] = userInputParams['beta2']
+    userInputParams['beta1'] = 0.7
+    userInputParams['beta2'] = 0.9
+    userInputParams['infinite'] = 2/3
+    userInputParams['linearity'] = 2/3
+
     # Compile additional input parameters.
     userInputParams = modelParameters.getNeuralParameters(userInputParams)
     modelConstants.updateModelParams(userInputParams)
     submodel = userInputParams['submodel']
-
-    # TODO
-    modelConstants.userInputParams['infinite'] = modelConstants.userInputParams['beta1']
-    modelConstants.userInputParams['linearity'] = modelConstants.userInputParams['beta2']
-    modelConstants.userInputParams['beta1'] = 0.7
-    modelConstants.userInputParams['beta2'] = 0.9
 
     # Initialize the model information classes.
     modelCompiler = compileModelData(submodel, userInputParams, useTherapyData=False, accelerator=accelerator)  # Initialize the model compiler.
