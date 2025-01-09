61162af8f669da498f278831a7cfeef3e66bb4fd
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-01-08 18:18:18 -0800

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -37,12 +37,12 @@ lrs_reversible=('1e-3')  # 1e-4 <= x == 1e-3; Removed from filename
 # Collective Switchables: 128
 #numSpecificEncoderLayers_arr=(1 2 4 8)
 #numSharedEncoderLayers_arr=(4 6 8 12 16 24 32)
-numSpecificEncoderLayers_arr=(2)
-numSharedEncoderLayers_arr=(12)
+numSpecificEncoderLayers_arr=(1)
+numSharedEncoderLayers_arr=(6)
 
 # Collective Switchables: 256
 encodedDimensions_arr=(256)
-allNumEncodedWeights=(32)
+allNumEncodedWeights=(32 64 128)
 #numProfileShots_arr=(24 16 8 32)
 numProfileShots_arr=(24)
 #lrs_profile=('0.005' '0.01' '0.02' '0.03' '0.04' '0.05' '0.06' '0.07' '0.08' '0.09' '0.1' '0.25' '0.5' '0.75' '1')  # 0.005 <= x <= 0.05
@@ -51,7 +51,7 @@ lrs_profile=('0.05')
 # Single Switchables.
 waveletTypes_arr=('bior3.1')
 #optimizers_arr=('NAdam' 'AdamW' 'RAdam' 'Adam')  # AdamW == NAdam > RAdam > Adam > Adamax
-optimizers_arr=('AdamW')
+optimizers_arr=('NAdam')
 
 
 for beta1s in "${beta1s_arr[@]}"
