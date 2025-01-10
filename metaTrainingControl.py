ab7a043392eda7b0b27ae84f600074c53abb7965
Samuel Solomon <sasolomon1998@gmail.com>
2025-01-09 17:16:54 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     parser.add_argument('--numSpecificEncoderLayers', type=int, default=2, help='The number of layers in the model.')
     parser.add_argument('--numSharedEncoderLayers', type=int, default=8, help='The number of layers in the model.')
     parser.add_argument('--numEncodedWeights', type=int, default=32, help='The number of profile weights.')
-    parser.add_argument('--numProfileShots', type=int, default=32, help='The epochs for profile training.')
+    parser.add_argument('--numProfileShots', type=int, default=24, help='The epochs for profile training.')
 
     # Add arguments for the emotion and activity architecture.
     parser.add_argument('--numBasicEmotions', type=int, default=6, help='The number of basic emotions (basis states of emotions).')
@@ -72,12 +72,12 @@ if __name__ == "__main__":
 
     # ----------------------- Training Parameters ----------------------- #
     # Signal encoder learning rates.
-    parser.add_argument('--profileLR', type=float, default=0.1, help='The learning rate of the health model.')
+    parser.add_argument('--profileLR', type=float, default=0.05, help='The learning rate of the health model.')
     parser.add_argument('--reversibleLR', type=float, default=1e-3, help='The learning rate of the general model.')
     parser.add_argument('--physGenLR', type=float, default=1e-4, help='The learning rate of the general model.')
 
     # Signal encoder weight decays.
-    parser.add_argument('--profileWD', type=float, default=1e-6, help='The learning rate of the general model, [1e-6, 1e-3]')
+    parser.add_argument('--profileWD', type=float, default=1e-3, help='The learning rate of the general model, [1e-6, 1e-3]')
     parser.add_argument('--reversibleWD', type=float, default=1e-4, help='The learning rate of the general model.')
     parser.add_argument('--physGenWD', type=float, default=1e-5, help='The learning rate of the general model.')
 
