212d6679979fbefeaa76170af92c5cede4cbda4e
Samuel Solomon <sasolomon1998@gmail.com>
2025-01-09 17:16:10 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
 
     # ----------------------- Training Parameters ----------------------- #
     # Signal encoder learning rates.
-    parser.add_argument('--profileLR', type=float, default=0.05, help='The learning rate of the health model.')
+    parser.add_argument('--profileLR', type=float, default=0.1, help='The learning rate of the health model.')
     parser.add_argument('--reversibleLR', type=float, default=1e-3, help='The learning rate of the general model.')
     parser.add_argument('--physGenLR', type=float, default=1e-4, help='The learning rate of the general model.')
 
