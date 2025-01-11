8f7c2eae5750b8447116591a6ed745e3c79dbd6a
Samuel Solomon <sasolomon1998@gmail.com>
2025-01-10 20:44:05 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     parser.add_argument('--physGenWD', type=float, default=1e-2, help='The learning rate of the general model.')
 
     # Add arguments for the emotion and activity architecture.
-    parser.add_argument('--momentum_decay', type=float, default=0.01, help='Momentum decay for the optimizer.')
+    parser.add_argument('--momentum_decay', type=float, default=0.001, help='Momentum decay for the optimizer.')
     parser.add_argument('--beta1', type=float, default=0.7, help='Beta1 for the optimizer: 0.7 -> 0.9')
     parser.add_argument('--beta2', type=float, default=0.999, help='Beta2 for the optimizer: 0.9 -> 0.999')
 
