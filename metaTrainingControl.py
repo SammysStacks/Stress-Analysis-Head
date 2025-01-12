4328dac5839318187500ab847658053b24fd401e
Samuel Solomon <sasolomon1998@gmail.com>
2025-01-12 14:06:21 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     parser.add_argument('--initialProfileAmp', type=float, default=0.01, help='The limits for profile initialization, [0.005, 0.05]')
     parser.add_argument('--numSpecificEncoderLayers', type=int, default=2, help='The number of layers in the model.')
     parser.add_argument('--numSharedEncoderLayers', type=int, default=6, help='The number of layers in the model.')
-    parser.add_argument('--profileDimension', type=int, default=128, help='The number of profile weights.')
+    parser.add_argument('--profileDimension', type=int, default=32, help='The number of profile weights.')
     parser.add_argument('--numProfileShots', type=int, default=24, help='The epochs for profile training.')
 
     # Add arguments for the emotion and activity architecture.
