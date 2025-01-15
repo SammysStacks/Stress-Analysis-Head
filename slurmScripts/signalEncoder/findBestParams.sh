5e27039305c6a07d5ebc35dd6c9718cb30d3d18f
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-01-14 21:27:00 -0800

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -2,24 +2,29 @@
 
 # Optimizer parameters.
 optimizers_arr=('NAdam')  # NAdam > AdamW > RAdam >= Adam > Adamax
-momentums_arr=('0.001')  # Removed from filename
-beta1s_arr=('0.7')  # Removed from filename
-beta2s_arr=('0.8')  # Removed from filename
+momentums_arr=('0.001')
+beta1s_arr=('0.7')
+beta2s_arr=('0.8')
 
 # Weight decay parameters.
-wds_profile=('0')  # 1e-6 ==> x <== 1e-3; Removed from filename
-wds_profileGen=('1e-4')  # 1e-5 == x <= 1e-4; Removed from filename
-wds_reversible=('1e-4')  # 1e-4 == x <= 1e-3; Removed from filename
+wds_profile=('0')  # 1e-6 ==> x <== 1e-3
+wds_profileGen=('0' '1e-4')  # 1e-5 == x <= 1e-4
+wds_reversible=('0' '1e-4')  # 1e-4 == x <= 1e-3
+
+# Learning parameters.
+lrs_profile=('0.05' '0.06' '0.07' '0.08' '0.09' '0.1')  # 0.005 <= x <= 0.05
+lrs_profileGen=('2.5e-4') # # 5e-5 <= x == 1e-4; Removed from filename
+lrs_reversible=('1e-4')  # 1e-4 <= x == 1e-3; Removed from filename
 
 # Known interesting parameters: 96
 numSharedEncoderLayers_arr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)  # 16
 numSpecificEncoderLayers_arr=(1 2 3 4 5 6)  # 6
 
 # Known interesting parameters: 144
-profileParams=(16 32 64 128 256 512)  # 6
+profileParams=(1 2 4 8 16 32 64 128 256 512)  # 
 numProfileShots_arr=(4 8 12 16 24 32)  # 6
-encodedDimensions_arr=(32 64 128 256 512)  # 4
-initialProfileAmp_arr=('0.01')  # 0.005 <= x <= 0.05
+encodedDimensions_arr=(16 32 64 128 256 512)  # 4
+initialProfileAmp_arr=('0.001')  # 0.005 <= x <= 0.05
 
 # Neural operator parameters.
 waveletTypes_arr=(
@@ -28,28 +33,20 @@ waveletTypes_arr=(
     'bior3.1' 'bior3.3' 'bior3.5' 'bior3.7' 'bior3.9' 'bior4.4' 'bior5.5' 'bior6.8' \
 )  # 'bior3.1' > 'bior3.3' > 'bior2.2' > 'bior3.5'
 
-# Learning parameters.
-lrs_profile=('0.001' '0.005' '0.01' '0.02' '0.03' '0.04' '0.05' '0.06' '0.07' '0.08' '0.09' '0.1')  # 0.005 <= x <= 0.05
-lrs_profileGen=('1e-4') # # 5e-5 <= x == 1e-4; Removed from filename
-lrs_reversible=('1e-3')  # 1e-4 <= x == 1e-3; Removed from filename
 
 # Finished
-encodedDimensions_arr=(128)
+encodedDimensions_arr=(256)
+numProfileShots_arr=(24)
+profileParams=(128)
+
+# Finished
 waveletTypes_arr=('bior3.1')
 
 # Collective Switchables: 20
 numSpecificEncoderLayers_arr=(1 2)
 numSharedEncoderLayers_arr=(2 4 6 8)
 #numSpecificEncoderLayers_arr=(1)
-#numSharedEncoderLayers_arr=(5)
-
-# Collective Switchables: 5
-#numProfileShots_arr=(32 20 16 12 8)
-numProfileShots_arr=(24)
-
-# Collective Switchables: 4
-#profileParams=(16 64 128 256)
-profileParams=(128)
+#numSharedEncoderLayers_arr=(6)
 
 # Collective Switchables: 90
 lrs_profile=('0.06' '0.067' '0.07' '0.75' '0.8')  # 0.005 <= x <= 0.05
@@ -58,17 +55,6 @@ lrs_profileGen=('1e-4' '2.5e-4') # # 1e-4 <= x == 1e-3; lrs_profileGen <= lrs_re
 #lrs_profile=('0.067')
 #lrs_reversible=('3e-4')
 #lrs_profileGen=('1e-4')
-
-# Single Switchables: 2
-optimizers_arr=('NAdam')  # AdamW == NAdam > RAdam > Adam > Adamax
-
-# Weight decay parameters.
-wds_profile=('0' '1e-2' '1e-3' '1e-4' '1e-5')  # 1e-6 ==> x <== 1e-3; Removed from filename
-wds_reversible=('1e-2' '1e-3' '1e-4')  # 1e-4 == x <= 1e-3; Removed from filename
-wds_profileGen=('1e-2' '1e-3' '1e-4')  # 1e-5 == x <= 1e-4; Removed from filename
-wds_profile=('0')
-wds_reversible=('1e-4')
-wds_profileGen=('1e-4')
 
 for beta1s in "${beta1s_arr[@]}"
 do
