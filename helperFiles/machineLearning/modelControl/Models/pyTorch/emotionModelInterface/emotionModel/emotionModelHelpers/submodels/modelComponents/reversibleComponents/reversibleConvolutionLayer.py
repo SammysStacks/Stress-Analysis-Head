7cb6d72c31c5b7dfa18b6d98b9178951a585dec2
Samuel Solomon <sasolomon1998@gmail.com>
2025-01-18 21:29:31 -0800

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/reversibleComponents/reversibleConvolutionLayer.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/reversibleComponents/reversibleConvolutionLayer.py
@@ -38,8 +38,8 @@ class reversibleConvolutionLayer(reversibleInterface):
         for layerInd in range(self.numLayers):
             # Create the neural weights.
             parameters = nn.Parameter(torch.randn(self.numSignals, self.numParams or 1, dtype=torch.float64))
-            parameters = nn.init.uniform_(parameters, a=-0.2, b=0.2)  # TODO ADD BACK?
-            # parameters = nn.init.normal_(parameters, mean=0, std=0.1)  # TODO ADD BACK?
+            # parameters = nn.init.uniform_(parameters, a=-0.2, b=0.2)  # TODO ADD BACK?
+            parameters = nn.init.normal_(parameters, mean=0, std=0.1)  # TODO ADD BACK?
             # parameters = nn.init.zeros_(parameters)  # TODO REMOVE
             # parameters = nn.init.kaiming_uniform_(parameters)  # TODO: OLD
             self.givensRotationParams.append(parameters)
