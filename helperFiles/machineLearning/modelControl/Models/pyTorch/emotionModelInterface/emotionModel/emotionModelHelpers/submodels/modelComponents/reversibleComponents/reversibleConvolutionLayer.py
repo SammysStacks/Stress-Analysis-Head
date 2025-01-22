5ac33da602e00fede26791bce2d5a28ffdd3238f
Samuel Solomon <sasolomon1998@gmail.com>
2025-01-21 22:27:13 -0800

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/reversibleComponents/reversibleConvolutionLayer.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/reversibleComponents/reversibleConvolutionLayer.py
@@ -40,9 +40,9 @@ class reversibleConvolutionLayer(reversibleInterface):
             # Create the neural weights.
             parameters = nn.Parameter(torch.randn(self.numSignals, self.numParams or 1, dtype=torch.float64))
 
-            # parameters = nn.init.normal_(parameters, mean=0, std=0.01)  # TODO ADD BACK?
+            parameters = nn.init.normal_(parameters, mean=0, std=0.01)  # TODO ADD BACK?
             # parameters = nn.init.uniform_(parameters, a=-0.01, b=0.01)  # TODO ADD BACK?
-            parameters = nn.init.zeros_(parameters)  # TODO REMOVE
+            # parameters = nn.init.zeros_(parameters)  # TODO REMOVE
 
             # parameters = nn.init.kaiming_normal_(parameters, a=0, mode='fan_in', nonlinearity='leaky_relu')
             # parameters = nn.init.kaiming_uniform_(parameters)  # TODO: OLD
