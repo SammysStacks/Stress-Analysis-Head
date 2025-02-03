427852caecee7d46d747657cd97ea974a9a4c9ba
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-02-02 19:37:23 -0800

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -23,7 +23,7 @@ cullingEpochs=(1 2 3 4 5 6 7 8 9 10)  # 10
 # ----- # TODO
 angularThresholdMins=(1 2 3 4 5 6 7 8 9 10)  # 9; 2 <= x <= 5
 angularThresholdMaxs=(45)  # 4; 20 <= x == (30, 45)
-cullingEpochs=(1 2 4 5 6 8 10)  # 10
+cullingEpochs=(1 2 3 4 5 6 7 8 9 10)  # 10
 
 # Known interesting parameters: 63
 numSharedEncoderLayers_arr=(0 1 2 3 4 5 6 7 8)  # 9
