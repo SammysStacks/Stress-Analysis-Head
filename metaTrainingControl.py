a1d6ec6531fe226214bbcaf7ae1990bdecfd745b
Samuel Solomon <sasolomon1998@gmail.com>
2025-02-08 13:27:42 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     parser.add_argument('--angularThresholdMin', type=float, default=3, help='The minimum rotational threshold in degrees.')
     parser.add_argument('--numSpecificEncoderLayers', type=int, default=2, help='The number of layers in the model: [1, 2]')
     parser.add_argument('--numSharedEncoderLayers', type=int, default=6, help='The number of layers in the model: [2, 8]')
-    parser.add_argument('--cullingEpoch', type=int, default=5, help='The number of epochs before culling null weights.')
+    parser.add_argument('--cullingEpoch', type=int, default=1, help='The number of epochs before culling null weights.')
     parser.add_argument('--profileDimension', type=int, default=128, help='The number of profile weights: [32, 256]')
     parser.add_argument('--numProfileShots', type=int, default=32, help='The epochs for profile training: [16, 32]')
 
