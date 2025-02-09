22573b50b1708b8970837fbaa59ef200e1292743
Samuel Solomon <sasolomon1998@gmail.com>
2025-02-08 17:34:15 -0800

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/sharedSignalEncoderModel.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/sharedSignalEncoderModel.py
@@ -36,7 +36,7 @@ class sharedSignalEncoderModel(neuralOperatorInterface):
 
     def addLayer(self):
         self.neuralLayers.append(self.getNeuralOperatorLayer(neuralOperatorParameters=self.neuralOperatorParameters, reversibleFlag=True))
-        if self.learningProtocol == 'rCNN': self.processingLayers.append(self.postProcessingLayerRCNN(numSignals=1, sequenceLength=self.encodedDimension))
+        if self.learningProtocol == 'rCNN': self.processingLayers.append(nn.Identity())  # self.postProcessingLayerRCNN(numSignals=1, sequenceLength=self.encodedDimension))
         elif self.learningProtocol == 'FC': self.processingLayers.append(self.postProcessingLayerFC(sequenceLength=self.encodedDimension))
         elif self.learningProtocol == 'CNN': self.processingLayers.append(self.postProcessingLayerCNN(numSignals=self.numSignals))
         else: raise "The learning protocol is not yet implemented."
