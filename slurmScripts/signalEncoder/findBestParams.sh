1b6d0e2094cdbe26528a844469aebc6232f532ec
Samuel Solomon <sasolomon1998@gmail.com>
2025-02-17 01:03:24 -0800

    Lab

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -51,22 +51,12 @@ waveletTypes_arr=(
 )
 
 # Angular reference states.
-<<<<<<< HEAD
 finalMinAngularThresholds=(1 2 3)  # [0, 5]; Best: [0, 3]
 percentParamsKeepings=(4 6 8)  # [5, 10]; Best: 8?
 cullingEpochs=(5 10)
 
 # Angular reference states.
 minAngularThresholds=('0.01' '0.1')
-=======
-minNumParameters_arr=(600 800 1000)
-finalMinAngularThresholds=(3 4)  # [0, 5]; Best: [0, 3]
-percentParamsKeepings=(8)  # [5, 10]; Best: 8?
-cullingEpochs=(5)
-
-# Angular reference states.
-minAngularThresholds=('0.05' '0.1')
->>>>>>> 5e24d7d5 (HPC)
 maxAngularThresholds=(45)
 
 # Binary reference states.
