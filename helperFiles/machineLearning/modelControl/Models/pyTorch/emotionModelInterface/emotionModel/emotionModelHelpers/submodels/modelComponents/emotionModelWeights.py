95230a8026b383998a7cef3915b262ca23bc0d9b
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-04 22:39:41 -0800

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/emotionModelWeights.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/emotionModelWeights.py
@@ -70,7 +70,9 @@ class emotionModelWeights(convolutionalHelpers):
         numUpSamples = int(math.log2(numOutputFeatures // modelConstants.userInputParams['profileDimension']))
 
         layers = []
-        for i in range(numUpSamples): layers.append(self.convolutionalFilters_resNetBlocks(numResNets=1, numBlocks=1, numChannels=[1, 2], kernel_sizes=1, dilations=1, groups=1, strides=1, convType='conv1D', activationMethod="SoftSign", numLayers=None, addBias=False))
+        for i in range(numUpSamples):
+            layers.append(self.convolutionalFilters_resNetBlocks(numResNets=4, numBlocks=4, numChannels=[1, 1], kernel_sizes=3, dilations=1, groups=1, strides=1, convType='conv1D', activationMethod="SoftSign", numLayers=None, addBias=False))
+            layers.append(self.convolutionalFilters_resNetBlocks(numResNets=1, numBlocks=1, numChannels=[1, 2], kernel_sizes=1, dilations=1, groups=1, strides=1, convType='conv1D', activationMethod="SoftSign", numLayers=None, addBias=False))
 
         # Best [2, 6]
         for _ in range(12):
