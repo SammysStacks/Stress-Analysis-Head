852c73ff8f146a09c33e612295cc44706cdee2f7
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-06 00:28:11 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -56,12 +56,12 @@ if __name__ == "__main__":
 
     # Add arguments for the signal encoder architecture.
     parser.add_argument('--numSpecificEncoderLayers', type=int, default=1, help='The number of layers in the model: [1, 2]')
-    parser.add_argument('--numSharedEncoderLayers', type=int, default=5, help='The number of layers in the model: [2, 10]')
+    parser.add_argument('--numSharedEncoderLayers', type=int, default=7, help='The number of layers in the model: [2, 10]')
 
     # Add arguments for the health profile.
     parser.add_argument('--initialProfileAmp', type=float, default=1e-3, help='The limits for profile initialization. Should be near zero.')
     parser.add_argument('--profileDimension', type=int, default=64, help='The number of profile weights: [32, 256]')
-    parser.add_argument('--numProfileShots', type=int, default=16, help='The epochs for profile training: [16, 32]')
+    parser.add_argument('--numProfileShots', type=int, default=20, help='The epochs for profile training: [16, 32]')
 
     # Add arguments for observational learning.
     parser.add_argument('--finalMinAngularThreshold', type=float, default=1, help='The final min rotational threshold in degrees.')
@@ -80,9 +80,9 @@ if __name__ == "__main__":
     # ----------------------- Training Parameters ----------------------- #
 
     # Signal encoder learning rates.
-    parser.add_argument('--profileLR', type=float, default=0.1, help='The learning rate of the health model.')
-    parser.add_argument('--physGenLR', type=float, default=5e-5, help='The learning rate of the general model.')
-    parser.add_argument('--reversibleLR', type=float, default=5e-4, help='The learning rate of the general model.')
+    parser.add_argument('--profileLR', type=float, default=0.075, help='The learning rate of the health model.')
+    parser.add_argument('--physGenLR', type=float, default=1e-5, help='The learning rate of the general model.')
+    parser.add_argument('--reversibleLR', type=float, default=4e-4, help='The learning rate of the general model.')
 
     # Signal encoder weight decays.
     parser.add_argument('--profileWD', type=float, default=0, help='The learning rate of the general model.')
@@ -98,7 +98,7 @@ if __name__ == "__main__":
 
     # Parse the arguments.
     userInputParams = vars(parser.parse_args())
-    userInputParams['numInitialEpochs'] = 25
+    userInputParams['numInitialEpochs'] = 100
     userInputParams['minWaveletDim'] = 32
 
     # Compile additional input parameters.
