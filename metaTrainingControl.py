cba4152c256341a40fd6b6e589bc65a7c6c22607
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-06 13:40:36 -0800

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -61,12 +61,12 @@ if __name__ == "__main__":
     # Add arguments for the health profile.
     parser.add_argument('--initialProfileAmp', type=float, default=1e-3, help='The limits for profile initialization. Should be near zero.')
     parser.add_argument('--profileDimension', type=int, default=64, help='The number of profile weights: [32, 256]')
-    parser.add_argument('--numProfileShots', type=int, default=20, help='The epochs for profile training: [16, 32]')
+    parser.add_argument('--numProfileShots', type=int, default=12, help='The epochs for profile training: [16, 32]')
 
     # Add arguments for observational learning.
     parser.add_argument('--finalMinAngularThreshold', type=float, default=1, help='The final min rotational threshold in degrees.')
     parser.add_argument('--maxNumParamsKeeping', type=int, default=5000, help='The percentage of parameters to keep in the model.')
-    parser.add_argument('--minAngularThreshold', type=float, default=0.05, help='The smaller rotational threshold in degrees.')
+    parser.add_argument('--minAngularThreshold', type=float, default=0.1, help='The smaller rotational threshold in degrees.')
     parser.add_argument('--maxAngularThreshold', type=float, default=45, help='The larger rotational threshold in degrees.')
 
     # dd arguments for the emotion and activity architecture.
