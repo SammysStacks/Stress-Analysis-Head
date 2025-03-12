28e5e36f4aa98cfbb2a8128f8a5c6d38f3c163db
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-03-12 13:11:47 -0700

    HEAD

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/reversibleComponents/reversibleLieLayer.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/reversibleComponents/reversibleLieLayer.py
@@ -123,7 +123,7 @@ class reversibleLieLayer(reversibleLieLayerInterface):
             givensAngles = self.getGivensAngles(layerInd).abs()  # Dim: numSignals, numParams
             sortedGivensAngles, sortedIndices = torch.sort(givensAngles, dim=-1)
             # sortedGivensAngles -> [0, 0.1, 0.2, ... pi/2]
-            maxEpochThreshold = 50 if 64 <= self.sequenceLength else 200
+            maxEpochThreshold = 200
 
             # Get the threshold.
             numParamsKeeping = (maxEpochThreshold - epoch) * self.numParams / maxEpochThreshold if epoch <= maxEpochThreshold else 0
