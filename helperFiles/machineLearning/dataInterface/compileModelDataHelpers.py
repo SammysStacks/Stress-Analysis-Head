b04101cdbd8eedc63a8a75d21da4d4660ca8c6a4
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-16 12:15:20 -0700

    Lab

--- helperFiles/machineLearning/dataInterface/compileModelDataHelpers.py
+++ helperFiles/machineLearning/dataInterface/compileModelDataHelpers.py
@@ -50,7 +50,7 @@ class compileModelDataHelpers:
         if userInputParams['encodedDimension'] < userInputParams['profileDimension']: raise Exception("The number of encoded weights must be less than the encoded dimension.")
 
         # Get the model information.
-        signalEncoderModelInfo = f"signalEncoder thresh[{userInputParams['minAngularThreshold']} {userInputParams['maxAngularThreshold']}] {userInputParams['optimizerType']} {userInputParams['numSharedEncoderLayers']}-shared specific-{userInputParams['numSpecificEncoderLayers']} LR{userInputParams['profileLR']}-{userInputParams['reversibleLR']}-{userInputParams['physGenLR']} profileParams{userInputParams['profileDimension']} numShots{userInputParams['numProfileShots']} encodedDim{userInputParams['encodedDimension']} {userInputParams['neuralOperatorParameters']['wavelet']['waveletType']}-{userInputParams['neuralOperatorParameters']['wavelet']['minWaveletDim']}"
+        signalEncoderModelInfo = f"signalEncoder thresh[{userInputParams['minAngularThreshold']} {userInputParams['maxAngularThreshold']}] {userInputParams['optimizerType']} {userInputParams['numSharedEncoderLayers']}-shared specific-{userInputParams['numSpecificEncoderLayers']} LR{userInputParams['profileLR']}-{userInputParams['reversibleLR']}-{userInputParams['physGenLR']} profileParams{userInputParams['profileDimension']} numShots{userInputParams['numProfileShots']} encodedDim{userInputParams['encodedDimension']} {userInputParams['neuralOperatorParameters']['wavelet']['waveletType']}-{userInputParams['minWaveletDim']}"
         emotionPredictionModelInfo = f"emotionPrediction on {userInputParams['deviceListed']} with {userInputParams['optimizerType']}"
 
         # Return the model information.
