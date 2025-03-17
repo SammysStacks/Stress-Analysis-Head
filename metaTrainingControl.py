e513a7ba92e0982ced553b98d9a94df099a6b61c
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-17 11:02:41 -0700

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     # Add arguments for the neural operator.
     parser.add_argument('--waveletType', type=str, default='bior3.1', help='The wavelet type for the wavelet transform: bior3.1, bior3.3, bior2.2, bior3.5')
     parser.add_argument('--operatorType', type=str, default='wavelet', help='The type of operator to use for the neural operator: wavelet')
-    parser.add_argument('--numSpecificDecompositions', type=int, default=3, help='The number of decompositions to use: [1, 2]')
+    parser.add_argument('--numSpecificDecompositions', type=int, default=2, help='The number of decompositions to use: [1, 2]')
     parser.add_argument('--minWaveletDim', type=int, default=32, help='The minimum dimension of the wavelet transform.')
 
     # Add arguments for the signal encoder architecture.
@@ -82,8 +82,8 @@ if __name__ == "__main__":
     # ----------------------- Training Parameters ----------------------- #
 
     # Signal encoder learning rates.
-    parser.add_argument('--profileLR', type=float, default=0.008, help='The learning rate of the health model.')
-    parser.add_argument('--physGenLR', type=float, default=5e-5, help='The learning rate of the general model.')
+    parser.add_argument('--profileLR', type=float, default=0.05, help='The learning rate of the health model.')
+    parser.add_argument('--physGenLR', type=float, default=1e-4, help='The learning rate of the general model.')
     parser.add_argument('--reversibleLR', type=float, default=5e-4, help='The learning rate of the general model.')
 
     # Signal encoder weight decays.
