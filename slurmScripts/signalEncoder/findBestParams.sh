9815cdc15f4717f2007721858196d0652bf3a025
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-03-19 19:34:40 -0700

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -7,9 +7,9 @@ beta1s_arr=('0.7')
 beta2s_arr=('0.8')
 
 # Learning parameters.
-lrs_profile=(0.01 0.025 0.05)  # 0.005 <= x <= 0.075
-lrs_profileGen=('5e-4') # x <= 1e-4;
-lrs_reversible=('5e-4')  # 1e-4 <= x == 1e-3 -> [2.5e-4, 5e-4]
+lrs_profile=(0.025 0.05)  # 0.005 <= x <= 0.075
+lrs_profileGen=('4e-4' '1e-4') # x <= 1e-4;
+lrs_reversible=('4e-4' '1e-4')  # 1e-4 <= x == 1e-3 -> [2.5e-4, 5e-4]
 
 # Neural operator parameters.
 waveletTypes_arr=(
@@ -35,7 +35,7 @@ waveletTypes_arr=(
 )
 
 # Angular reference states.
-minAngularThresholds=(0.01 0.05 0.1)  # [0.01, 0.25]
+minAngularThresholds=(0.01 0.05)  # [0.01, 0.25]
 maxAngularThresholds=(45)
 
 # Binary reference states.
@@ -43,14 +43,14 @@ numSpecificEncoderLayers_arr=(1)
 numSharedEncoderLayers_arr=(5)  # [4, 10]; Best: 5 and 7
 
 # Profile parameters.
-numProfileShots_arr=(24)  # (8, [16, 24], 32)
+numProfileShots_arr=(16)  # (8, [16, 24], 32)
 encodedDimensions_arr=(512)
-profileParams=(128)  #######
+profileParams=(128 256)  #######
 
 # Wavelet states.
-numSpecificDecompositions_arr=(2 3)
-waveletTypes_arr=('bior3.1' 'bior3.3' 'bior3.5' 'bior3.7')
-minWaveletDims=(128 64)   ########
+numSpecificDecompositions_arr=(1 2 3)
+waveletTypes_arr=('bior3.1')
+minWaveletDims=(32)   ########
 
 for minAngularThreshold in "${minAngularThresholds[@]}"
 do
