548ed1fa5f62480c21b253442ba8fea49e1c71cf
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-21 22:22:59 -0700

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/emotionModelWeights.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/emotionModelWeights.py
@@ -65,7 +65,7 @@ class emotionModelWeights(convolutionalHelpers):
 
     def healthGeneration(self):
         layers = []
-        for _ in range(16):  # [4, 12]
+        for _ in range(16):
             layers.append(self.convolutionalFilters_resNetBlocks(numResNets=3, numBlocks=3, numChannels=[1, 1], kernel_sizes=3, dilations=[1, 2, 1], groups=1, strides=1, convType='conv1D', activationMethod="SoftSign", numLayers=None, addBias=False))
             layers.append(self.convolutionalFilters_resNetBlocks(numResNets=3, numBlocks=3, numChannels=[1, 1], kernel_sizes=3, dilations=[1, 4, 1], groups=1, strides=1, convType='conv1D', activationMethod="SoftSign", numLayers=None, addBias=False))
 
@@ -74,7 +74,7 @@ class emotionModelWeights(convolutionalHelpers):
 
     def fourierAdjustments(self):
         layers = []
-        for _ in range(2):  # [4, 12]
+        for _ in range(6):
             layers.append(self.convolutionalFilters_resNetBlocks(numResNets=3, numBlocks=3, numChannels=[2, 2], kernel_sizes=3, dilations=[1, 2, 1], groups=1, strides=1, convType='conv1D', activationMethod="SoftSign", numLayers=None, addBias=False))
             layers.append(self.convolutionalFilters_resNetBlocks(numResNets=3, numBlocks=3, numChannels=[2, 2], kernel_sizes=3, dilations=[1, 4, 1], groups=1, strides=1, convType='conv1D', activationMethod="SoftSign", numLayers=None, addBias=False))
 
