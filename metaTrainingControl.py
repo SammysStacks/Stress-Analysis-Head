7f1b3b1df45760c65c6fe6f2ae850ca0dfb4ca14
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-26 07:18:34 -0700

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     # Add arguments for the neural operator.
     parser.add_argument('--waveletType', type=str, default='bior3.1', help='The wavelet type for the wavelet transform: bior3.1, bior3.3, bior2.2, bior3.5')
     parser.add_argument('--operatorType', type=str, default='wavelet', help='The type of operator to use for the neural operator: wavelet')
-    parser.add_argument('--minWaveletDim', type=int, default=64, help='The minimum dimension of the wavelet transform')
+    parser.add_argument('--minWaveletDim', type=int, default=32, help='The minimum dimension of the wavelet transform')
 
     # Add arguments for the signal encoder architecture.
     parser.add_argument('--numSpecificEncoderLayers', type=int, default=1, help='The number of layers in the model: [1, 2]')
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     # Add arguments for observational learning.
     parser.add_argument('--maxAngularThreshold', type=float, default=45, help='The larger rotational threshold in (degrees)')
     parser.add_argument('--minAngularThreshold', type=float, default=4, help='The smaller rotational threshold in (degrees)')
-    parser.add_argument('--minThresholdStep', type=float, default=0.2, help='The rotational threshold step (degrees)')
+    parser.add_argument('--minThresholdStep', type=float, default=0.1, help='The rotational threshold step (degrees)')
 
     # dd arguments for the emotion and activity architecture.
     parser.add_argument('--numBasicEmotions', type=int, default=6, help='The number of basic emotions (basis states of emotions)')
@@ -83,9 +83,9 @@ if __name__ == "__main__":
     # ----------------------- Training Parameters ----------------------- #
 
     # Signal encoder learning rates.
-    parser.add_argument('--profileLR', type=float, default=0.01, help='The learning rate of the profile')
-    parser.add_argument('--physGenLR', type=float, default=1e-4, help='The learning rate of the profile generation (CNNs)')
-    parser.add_argument('--reversibleLR', type=float, default=0.02, help='The learning rate of the Lie manifold angles (degrees)')
+    parser.add_argument('--profileLR', type=float, default=0.02, help='The learning rate of the profile')
+    parser.add_argument('--physGenLR', type=float, default=2e-4, help='The learning rate of the profile generation (CNNs)')
+    parser.add_argument('--reversibleLR', type=float, default=0.025, help='The learning rate of the Lie manifold angles (degrees)')
 
     # Add arguments for the emotion and activity architecture.
     parser.add_argument('--momentum_decay', type=float, default=0.001, help='Momentum decay for the optimizer')
