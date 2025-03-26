308e88683e1ee31d02c075d192efd96a25c7b0f0
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-25 19:53:31 -0700

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     # Signal encoder learning rates.
     parser.add_argument('--profileLR', type=float, default=0.01, help='The learning rate of the profile')
     parser.add_argument('--physGenLR', type=float, default=1e-4, help='The learning rate of the profile generation (CNNs)')
-    parser.add_argument('--reversibleLR', type=float, default=0.025, help='The learning rate of the Lie manifold angles (degrees)')
+    parser.add_argument('--reversibleLR', type=float, default=0.02, help='The learning rate of the Lie manifold angles (degrees)')
 
     # Add arguments for the emotion and activity architecture.
     parser.add_argument('--momentum_decay', type=float, default=0.001, help='Momentum decay for the optimizer')
