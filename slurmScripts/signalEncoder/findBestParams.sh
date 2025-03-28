a8bc9bfca80189f2197ef08f0be84e6605ba939b
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-03-28 04:43:08 -0400

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -49,22 +49,23 @@ minAngularThresholds=(4)  # [0.01, 0.25]
 
 lrs_profile=(0.01)  # 0.005 <= x <= 0.075
 lrs_profileGen=('2e-4' '4e-4' '1e-3') # x <= 1e-4;
-lrs_reversible=(0.05 0.025 0.01)  # 4e-4 <= x == 1e-3
-minThresholdSteps=(0.01 0.025 0.05)
-minAngularThresholds=(4)  # [0.01, 0.25]
+lrs_reversible=(0.075 0.05 0.025)  # 4e-4 <= x == 1e-3
+minThresholdSteps=(0.025 0.05 0.075 0.1)
+minAngularThresholds=(2)  # [0.01, 0.25]
 
 # Binary reference states.
 numSpecificEncoderLayers_arr=(1)
-numSharedEncoderLayers_arr=(5)  # [4, 10]; Best: 5 and 7
+numSharedEncoderLayers_arr=(7)  # [4, 10]; Best: 5 and 7
 
 # Profile parameters.
 numProfileShots_arr=(24)  # (8, [16, 24], 32)
 encodedDimensions_arr=(512)
-profileParams=(64 128)  # [64, 128]
+profileParams=(32 64 128)  # [64, 128]
 
 # Wavelet states.
 waveletTypes_arr=('bior3.1')
 minWaveletDims=(32 64 128)  # [64, 128]
+minWaveletDims=(32)  # [64, 128]
 
 for minThresholdStep in "${minThresholdSteps[@]}"
 do
