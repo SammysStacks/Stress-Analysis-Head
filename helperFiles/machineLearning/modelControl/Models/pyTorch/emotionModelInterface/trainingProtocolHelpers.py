112c6122ecf0236291cbf20765fe98aefc08b4b5
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-28 09:51:00 -0700

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/trainingProtocolHelpers.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/trainingProtocolHelpers.py
@@ -64,13 +64,14 @@ class trainingProtocolHelpers:
 
             if not onlyProfileTraining:
                 # Train the updated model.
-                modelPipeline.trainModel(dataLoader, submodel, profileTraining=False, specificTraining=True, trainSharedLayers=False, stepScheduler=False, numEpochs=numEpochs)  # Signal-specific training.
                 modelPipeline.model.cullAngles(epoch=epoch)
+                modelPipeline.trainModel(dataLoader, submodel, profileTraining=False, specificTraining=True, trainSharedLayers=False, stepScheduler=False, numEpochs=numEpochs)  # Signal-specific training.
+                # modelPipeline.model.cullAngles(epoch=epoch)
 
             # Health profile training.
             numProfileShots = modelPipeline.resetPhysiologicalProfile(submodel) if epoch != 0 else modelConstants.initialProfileEpochs
             modelPipeline.trainModel(dataLoader, submodel, profileTraining=True, specificTraining=False, trainSharedLayers=False, stepScheduler=True, numEpochs=numProfileShots + 1)  # Profile training.
-            self.accelerator.wait_for_everyone()
+            if not onlyProfileTraining: modelPipeline.model.cullAngles(epoch=epoch)
 
     def calculateLossInformation(self, allMetadataLoaders, allMetaModels, allModels, allDataLoaders, submodel):
         self.unifyAllModelWeights(allMetaModels, allModels)  # Unify all the model weights.
