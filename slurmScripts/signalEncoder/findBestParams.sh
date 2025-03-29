e3c6c4ab6a3774db96938ec77c634f2ec3fc8b2e
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-03-29 09:19:41 -0400

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -44,8 +44,8 @@ lrs_profile=(0.01)  # 0.005 <= x <= 0.075
 lrs_profileGen=('4e-4') # x <= 1e-4;
 lrs_reversible=(0.025)  # 4e-4 <= x == 1e-3
 
-#minThresholdSteps=(0.05)
-minAngularThresholds=(2.5)  # [0.01, 0.25]
+minThresholdSteps=(0.05)
+#minAngularThresholds=(2.5)  # [0.01, 0.25]
 waveletTypes_arr=('bior3.1')
 
 #lrs_profile=(0.01)  # 0.005 <= x <= 0.075
