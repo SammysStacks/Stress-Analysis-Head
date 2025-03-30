420a4b33901fc8bf7bcdcee5d3ccb9d9222c42a1
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-29 18:22:27 -0700

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/sharedSignalEncoderModel.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/sharedSignalEncoderModel.py
@@ -23,7 +23,7 @@ class sharedSignalEncoderModel(neuralOperatorInterface):
 
         # Only apply a transformation to the lowest of the high frequency decompositions.
         numDecompositions = waveletNeuralHelpers.max_decompositions(sequenceLength=modelConstants.userInputParams['encodedDimension'], waveletType=modelConstants.userInputParams['neuralOperatorParameters']['wavelet']['waveletType'], minWaveletDim=modelConstants.userInputParams['minWaveletDim']).item()
-        self.neuralOperatorParameters['wavelet']['encodeHighFrequencyProtocol'] = f'highFreq-{1}-{numDecompositions}'
+        self.neuralOperatorParameters['wavelet']['encodeHighFrequencyProtocol'] = f'highFreq-{0}-{numDecompositions-1}'
 
         # Initialize the pseudo-encoded times for the fourier data.
         hyperSampledTimes = torch.linspace(start=0, end=self.encodedTimeWindow, steps=self.encodedDimension).flip(dims=[0])
