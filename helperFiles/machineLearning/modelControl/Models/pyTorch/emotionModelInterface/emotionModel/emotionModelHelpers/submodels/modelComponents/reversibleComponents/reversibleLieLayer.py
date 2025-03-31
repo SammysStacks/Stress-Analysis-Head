91474104c4d72e03180b97bd3af95766b0a4a445
Samuel Solomon <sasolomon1998@gmail.com>
2025-03-31 14:31:28 -0700

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/reversibleComponents/reversibleLieLayer.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/reversibleComponents/reversibleLieLayer.py
@@ -108,8 +108,7 @@ class reversibleLieLayer(reversibleLieLayerInterface):
         # Get the minimum angular threshold.
         minThresholdStep = modelConstants.userInputParams['minThresholdStep']
         minAngularThreshold = modelConstants.userInputParams['minAngularThreshold']
-        minAngularThreshold = min(minAngularThreshold, (epoch + 10) * minThresholdStep) * torch.pi/180
-        print((epoch + 10) * minThresholdStep)
+        minAngularThreshold = min(minAngularThreshold, (epoch + 4) * minThresholdStep) * torch.pi/180
 
         return minAngularThreshold
 
