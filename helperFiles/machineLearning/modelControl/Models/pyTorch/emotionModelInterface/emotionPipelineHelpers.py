f5d59675eea7c83b042d53b591f007ce0edf111e
Samuel Solomon <sasolomon1998@gmail.com>
2025-04-02 01:31:51 -0700

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionPipelineHelpers.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionPipelineHelpers.py
@@ -57,7 +57,7 @@ class emotionPipelineHelpers:
         numEpochs = self.getTrainingEpoch(submodel) + 1  # +1 to account for the current epoch.
 
         # Get the current number of epochs for the profile model.
-        numProfileShots = min(max(3, numEpochs//2), modelParameters.getProfileEpochs())
+        numProfileShots = min(max(3, numEpochs), modelParameters.getProfileEpochs())
 
         # Reset and get the parameters that belong to the profile model
         self.model.specificSignalEncoderModel.profileModel.resetProfileHolders(numProfileShots)
