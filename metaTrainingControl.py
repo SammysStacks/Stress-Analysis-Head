ca0e1fa244070f31467fe76b4966e5cb383f18cd
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-04-01 18:38:31 -0700

    HEAD

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     # General model parameters.
     trainingDate = "2025-04-01"  # The current date we are training the model. Unique identifier of this training set.
-    plotAllEpochs = False  # Whether to plot all data every epoch (plotting once every numEpoch_toPlot regardless).
+    plotAllEpochs = True  # Whether to plot all data every epoch (plotting once every numEpoch_toPlot regardless).
     validationRun = False  # Whether to train new datasets from the old model.
     testSplitRatio = 0.1  # The percentage of testing points.
 
