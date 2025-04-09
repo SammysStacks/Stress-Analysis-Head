01bb165b17e16e6055f14a09873abb9f21e309f6
Samuel Solomon <sasolomon1998@gmail.com>
2025-04-09 12:15:56 -0700

    Lab

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/optimizerMethods/optimizerMethods.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/optimizerMethods/optimizerMethods.py
@@ -8,7 +8,7 @@ class optimizerMethods:
 
     def addOptimizer(self, submodel, model):
         angularWD, angularLR = modelConstants.userInputParams['reversibleLR']/10, modelConstants.userInputParams['reversibleLR']
-        if submodel == modelConstants.emotionModel: angularLR, angularWD = angularLR/5, angularWD/5
+        if submodel == modelConstants.emotionModel: angularWD, angularLR = angularWD*10/5, angularLR/5
 
         # Get the model parameters.
         modelParams = [
