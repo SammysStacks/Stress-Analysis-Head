df0fc1fee3c6cd512b4614f845272dd80e04db7d
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-04-10 19:53:27 -0700

    HEAD

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/lossInformation/organizeTrainingLosses.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/lossInformation/organizeTrainingLosses.py
@@ -179,7 +179,6 @@ class organizeTrainingLosses(lossCalculations):
         # Convert to class weights.
         class_weights = 1 / classCounts
         class_weights[torch.isnan(class_weights)] = 0
-        # class_weights = class_weights / class_weights.mean()  # TODO: Mean is for cross entropy weights.
-        class_weights = class_weights / class_weights.sum()  # TODO: Mean is for cross entropy weights.
+        class_weights = class_weights / class_weights.sum()  # TODO: Mean or sum is for cross entropy weights.
 
         return class_weights
