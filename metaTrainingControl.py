3b8f9b85358e68f8a3694dce018d9dc9f1f6a595
Samuel Solomon <sasolomon1998@gmail.com>
2025-04-12 03:11:42 -0700

    Lab

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -38,14 +38,14 @@ if __name__ == "__main__":
     )
 
     # General model parameters.
-    trainingDate = "2025-04-05 None"  # The current date we are training the model. Unique identifier of this training set.
+    trainingDate = "2025-04-12"  # The current date we are training the model. Unique identifier of this training set.
     unifyModelWeights = True  # Whether to unify the model weights across all models.
-    plotAllEpochs = False  # Whether to plot all data every epoch (plotting once every numEpoch_toPlot regardless).
+    plotAllEpochs = True  # Whether to plot all data every epoch (plotting once every numEpoch_toPlot regardless).
     validationRun = False  # Whether to train new datasets from the old model.
     testSplitRatio = 0.1  # The percentage of testing points.
 
     # Model loading information.
-    loadSubmodelDate = "2025-04-03"  # The submodel we are loading: None, "2025-03-31"
+    loadSubmodelDate = "2025-04-03---"  # The submodel we are loading: None, "2025-03-31"
 
     # ----------------------- Architecture Parameters ----------------------- #
 
