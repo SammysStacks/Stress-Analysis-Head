138bfd810c6f7da90a1b05c750ad462ad554d0a3
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-07-07 11:45:59 -0400

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -35,16 +35,16 @@ lrs_profileGen=('4e-4') # x <= 1e-4;
 lrs_reversible=(0.05)  # [0.025, 0.075]
 
 # Angular reference states.
-minAngularThresholds=(0.1)  # 0.2 - 0.5 - 1
-maxAngularThresholds=(2 4 6 8)  # 10, 45, 90
+minAngularThresholds=(0.5 0.75 1)  # 0.2 - 0.5 - 1
+maxAngularThresholds=(10 20 30 45)  # [10, 45, 90]; Absolute min [6, 10]
 
 # Binary reference states.
 numSpecificEncoderLayers_arr=(1)
-numSharedEncoderLayers_arr=(9 12 13 17)  # [4, 10]; Best: 5 and 7
+numSharedEncoderLayers_arr=(5 7 9)  # [4, 10]; Best: 5 and 7
 
 # Profile parameters.
 numProfileShots_arr=(32)  # (8, [16, 24], 32)
-encodedDimensions_arr=(256)  # [128, 256, 512]
+encodedDimensions_arr=(256 512)  # [128, 256, 512]
 
 # Wavelet states.
 waveletTypes_arr=('bior3.1')
