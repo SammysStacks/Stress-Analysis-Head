d0e6cb1b05c19f8c0158b35451a22eb019ae113d
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-07-13 10:29:00 -0400

    HEAD

--- helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelVisualizations/_signalEncoderVisualizations.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/emotionModelInterface/emotionModel/emotionModelHelpers/modelVisualizations/_signalEncoderVisualizations.py
@@ -454,7 +454,7 @@ class signalEncoderVisualizations(globalPlottingProtocols):
 
             # Create a meshgrid for encodedDimension and numModelLayers
             x_data, y_data = np.meshgrid(np.flip(dataTimes), np.flip(np.arange(1, 1 + numModelLayers), axis=-1))
-            x, y, z = x_data.flatten(), y_data.flatten(), dataStates.flatten()
+            x, y, z = np.flip(x_data.flatten()), np.flip(y_data.flatten()), dataStates.flatten()
 
             # Figure and axis settings
             fig = plt.figure(figsize=(12, 8), facecolor="white")
