493b16b2c280acb4a425a92d2eeccdbbd3bef338
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2025-07-20 08:16:49 -0600

    HEAD

--- slurmScripts/signalEncoder/findBestParams.sh
+++ slurmScripts/signalEncoder/findBestParams.sh
@@ -31,7 +31,7 @@ waveletTypes_arr=(
 
 # Learning parameters.
 lrs_profile=(0.01)  # 0.005 <= x <= 0.05
-lrs_profileGen=('4e-4' '1e-4' '1e-3') # x <= 1e-4;
+lrs_profileGen=('4e-4') # x <= 1e-4;
 lrs_reversible=(0.05)  # [0.025, 0.075]
 
 # Angular reference states.
