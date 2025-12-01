609749f3b1cb3977e25048265facfe08c5df9d81
liurxiao@gmail.com <Liurxiao@gmail.com>
2025-12-01 15:37:52 -0800

    RevisionTherapy

--- mainControl.py
+++ mainControl.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     reverseOrder = False  # Reverse the order of the data for training.
 
     # Specify the user parameters.
-    userName = "Selene".replace(" ", "")
-    trialName = "BinauralBeatsOClosedLoop"  # Experiment Type: Music ....
+    userName = "Huijie".replace(" ", "")
+    trialName = "BinauralBeatsOpenLoop"  # Experiment Type: Music ....
     date = "2025-12-01"
 
     # Specify experimental parameters.
